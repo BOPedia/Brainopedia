b1fb56105f4a968457ff33c8e6579468a6ae37b1
BOPedia <neurodivergencecampus@gmail.com>
2026-01-28 19:37:25 -0500

    Replace React Router App with working single-page app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Layout from './components/Layout'
+import React, { useState, useEffect } from 'react';
+import { Sidebar } from './components/Sidebar';
+import { ArticleContent } from './components/ArticleContent';
+import { Header } from './components/Header';
+import { DonationBanner } from './components/DonationBanner';
+import { Footer } from './components/Footer';
 
-function App() {
+export default function App() {
+  const [currentArticle, setCurrentArticle] = useState('about');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [currentArticle]);
+
+  useEffect(() => {
+    document.title = 'Brainopedia - Encyclopedia of Neurodivergent Conditions';
+  }, []);
+
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Layout>
-    </Router>
-  )
+    <div className="min-h-screen bg-[#2abcd4]">
+      <Header 
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
+      />
+      
+      <DonationBanner onNavigateToDonate={() => setCurrentArticle('donate')} />
+      
+      <div className="flex">
+        <Sidebar 
+          currentArticle={currentArticle}
+          setCurrentArticle={setCurrentArticle}
+          isOpen={isSidebarOpen}
+          closeSidebar={() => setIsSidebarOpen(false)}
+        />
+        
+        <main className="flex-1 p-2 md:p-8 lg:p-12 max-w-5xl mx-auto w-full">
+          <ArticleContent articleId={currentArticle} setCurrentArticle={setCurrentArticle} />
+        </main>
+      </div>
+      
+      <Footer setCurrentArticle={setCurrentArticle} />
+    </div>
+  );
 }
-
-export default App
