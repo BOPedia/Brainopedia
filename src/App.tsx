6e2e611a9e5b6f676806bf61d85602951a50e1a0
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-04-07 11:42:08 -0400

    fix: use react-router-dom for BrowserRouter import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useParams, useNavigate, useLocation } from 'react-router';
+import { BrowserRouter as Router, Routes, Route, useParams, useNavigate, useLocation } from 'react-router-dom';
 import { Sidebar } from './components/Sidebar';
 import { ArticleContent } from './components/ArticleContent';
 import { Header } from './components/Header';
