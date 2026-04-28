0e656b54ee6d489b5fbe6fff8c6aa8d3744a905c
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-04-27 23:12:32 -0400

    fixed mapping of articlecontent

--- src/components/ArticleContent.tsx
+++ src/components/ArticleContent.tsx
@@ -49,11 +49,27 @@ const articleMap: Record<string, React.ComponentType<ArticleProps>> = {
   // AUTISM SUB-PAGES
   'autism-overview': lazy(() => import('./articles/autism/ASDOverview').then(m => ({ default: m.ASDOverview }))),
   'autism-symptoms': lazy(() => import('./articles/autism/asd-symptoms/ASDSymptoms').then(m => ({ default: m.ASDSymptoms }))),
+  // --- THE MANAGER (Brings your tabs back!) ---
+  'autism-support': lazy(() => import('./articles/autism/asd-support/ASDSupport').then(m => ({ default: m.ASDSupport }))),
+
+  // --- THE CHILDREN (Files inside the support tabs) ---
+  'autism-early-intervention': lazy(() => import('./articles/autism/asd-support/EarlyInterventionContent').then(m => ({ default: m.EarlyInterventionContent }))),
+  'autism-therapies': lazy(() => import('./articles/autism/asd-support/TherapiesContent').then(m => ({ default: m.TherapiesContent }))),
+  'autism-educational': lazy(() => import('./articles/autism/asd-support/EducationalContent').then(m => ({ default: m.EducationalContent }))),
+  'autism-medical': lazy(() => import('./articles/autism/asd-support/MedicalContent').then(m => ({ default: m.MedicalContent }))),
+  'autism-environmental': lazy(() => import('./articles/autism/asd-support/EnvironmentalContent').then(m => ({ default: m.EnvironmentalContent }))),
+  'autism-family': lazy(() => import('./articles/autism/asd-support/FamilyPrinciplesContent').then(m => ({ default: m.FamilyPrinciplesContent }))),
+
+  // --- THE DEEP DIVES (The individual therapy pages) ---
+  'autism-occupational-therapy': lazy(() => import('./articles/autism/asd-support/OccupationalTherapy').then(m => ({ default: m.OccupationalTherapy }))),
+  'autism-aba-therapy': lazy(() => import('./articles/autism/asd-support/ABATherapy').then(m => ({ default: m.ABATherapy }))),
+  'autism-social-skills': lazy(() => import('./articles/autism/asd-support/SocialSkills').then(m => ({ default: m.SocialSkills }))),
+  'autism-mental-health': lazy(() => import('./articles/autism/asd-support/MentalHealth').then(m => ({ default: m.MentalHealth }))),
+  'autism-physical-therapy': lazy(() => import('./articles/autism/asd-support/PhysicalTherapy').then(m => ({ default: m.PhysicalTherapy }))),
   'autism-causes': lazy(() => import('./articles/autism/asd-causes/ASDCauses').then(m => ({ default: m.ASDCauses }))),
   'autism-diagnosis': lazy(() => import('./articles/autism/asd-diagnosis/ASDDiagnosis').then(m => ({ default: m.ASDDiagnosis }))),
   'autism-living': lazy(() => import('./articles/autism/asd-living/ASDLiving').then(m => ({ default: m.ASDLiving }))),
-  'autism-support': lazy(() => import('./articles/autism/asd-support/TherapiesContent').then(m => ({ default: m.TherapiesContent }))),
-
+  
   // MISC
   'about': lazy(() => import('./articles/ArticleAbout').then(m => ({ default: m.ArticleAbout }))),
   'donate': lazy(() => import('./articles/ArticleDonate').then(m => ({ default: m.ArticleDonate }))),
