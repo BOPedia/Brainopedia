ba204166224c3a6e8c41256536c2724774aa0385
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-04-29 13:08:53 -0400

    fix: trying svg for clarity purposes

--- src/components/articles/autism/asd-symptoms/CoreSymptoms.tsx
+++ src/components/articles/autism/asd-symptoms/CoreSymptoms.tsx
@@ -33,7 +33,7 @@ export function CoreSymptoms() {
                 {/* Venn Diagram Image - moved INSIDE a wrapper div */}
         <div className="flex justify-center mt-6">
           <ImageWithFallback 
-            src="/images/autism/autism-symptoms-Coretab-2core-venn.png"
+            src="/images/autism/autism-symptoms-Coretab-2core-venn.svg"
             alt="AUTISM Diagnostic Criteria Venn Diagram"
             className="w-full max-w-xl mx-auto mb-4 rounded"
           />
