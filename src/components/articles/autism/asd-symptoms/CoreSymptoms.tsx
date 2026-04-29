589f6f2e764ddcae23239ea44a8dd0bfb05bfb45
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-04-29 13:19:05 -0400

    size of core venn

--- src/components/articles/autism/asd-symptoms/CoreSymptoms.tsx
+++ src/components/articles/autism/asd-symptoms/CoreSymptoms.tsx
@@ -35,7 +35,7 @@ export function CoreSymptoms() {
           <ImageWithFallback 
             src="/images/autism/autism-symptoms-Coretab-2core-venn.svg"
             alt="AUTISM Diagnostic Criteria Venn Diagram"
-            className="w-full max-w-xl mx-auto mb-4 rounded"
+            className="w-full max-w-2xl mx-auto mb-4 rounded"
           />
         </div>
         
