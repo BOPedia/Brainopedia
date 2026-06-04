2e9862e8bf9fa08a65731978de06871ad975e500
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-06-03 21:16:26 -0400

    references title fixed

--- src/components/articles/adhd/ADHDSymptomsTabMore.tsx
+++ src/components/articles/adhd/ADHDSymptomsTabMore.tsx
@@ -656,7 +656,7 @@ export function ADHDSymptomsTabMore({ setCurrentArticle }: TabMoreProps) {
       <hr className="border-t-2 border-[#0c264d] border-opacity-10 my-8 clear-both" />
       
       <div className="bg-white bg-opacity-50 p-6 rounded-lg shadow-inner">
-        <h3 className="font-bold mb-5 text-xl font-spartan text-[#0c264d]">References: Expanded Lived Experience</h3>
+        <h3 className="font-bold mb-5 text-xl font-spartan text-[#0c264d]">References</h3>
         
         {/* CITED STUDIES: GREEN */}
         <div className="mb-6">
