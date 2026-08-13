ea834be6b002235f6e647249d8b98d6907e91498
Stephanie Walsh Tharp <neurodivergencecampus@gmail.com>
2026-08-13 11:01:34 -0400

    setup HelmetProvider and LCP image preloads

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async' // 1. Import the provider
 import App from './App.tsx'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* 2. Wrap your App in the provider */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
